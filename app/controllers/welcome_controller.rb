c294736b0dfefe23d8f0b67b3fb74105f0eb168b
Sharmi <rai.sharmistha@gmail.com>
2016-02-29 11:00:39 +0100

    Added comment line to be able to submit assignment again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,9 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
+
 end
+# add line to try to commit again as not all files were committed for some reason.
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   root 'welcome#index'
 
 #Having trouble merging so adding this line in
-
+# add line to try to commit again as not all files were committed for some reason.
 end
