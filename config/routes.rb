d7fa9ffc173b32502cfd026ae26f78ee563b9d1f
Sharmi <rai.sharmistha@gmail.com>
2016-03-10 12:31:26 +0100

    Checkpoint 34 Topics: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
   # instead of line above, also generated intiially as:
   # get "posts/index"
   # get "posts/show"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) {Post.create!(title: "New Post Title", body: "New Post Body") }
+
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+# #4
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+  it { is_expected.to belong_to(:topic) }
 
   describe "attributes" do
     it "responds to title" do
@@ -12,5 +17,5 @@ RSpec.describe Post, type: :model do
       expect(post).to respond_to(:body)
     end
   end
-  
+
 end
