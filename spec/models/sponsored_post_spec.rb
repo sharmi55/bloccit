f2368bb3dfb3859c2179087d643d55e8ebf9139c
Sharmi <rai.sharmistha@gmail.com>
2016-03-13 10:26:38 +0100

    Create model and associate to Topic model

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPost, type: :model do
-  let(:sponsored_post) { SponsoredPost.create!(title: "New Sponsored Post Title", body: "New Sponsored Post Body") }
 
-  describe "attributes" do
-# #2
-    it "responds to title" do
-      expect(sponsored_post).to respond_to(:title)
-    end
-# #3
-    it "responds to body" do
-      expect(sponsored_post).to respond_to(:body)
-    end
+   let (:sponsored_post) {SponsoredPost.create!}
 
-    it "responds to price" do
-      expect(sponsored_post).to respond_to(:price)
-    end
+     describe "attributes" do
+
+       it "responds to title" do
+         expect(sponsored_post).to respond_to(:title)
+       end
+
+       it "responds to bopy" do
+         expect(sponsored_post).to respond_to(:body)
+      end
+
+       it "responds to price" do
+        expect(sponsored_post).to respond_to(:price)
+       end
+     end
   end
-end
