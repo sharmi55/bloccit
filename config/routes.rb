8b7584329c0e1ed3ee0144b89d7d0457a97db7bc
Sharmi <rai.sharmistha@gmail.com>
2016-03-15 17:17:22 +0100

    Checkpoint 38 signin: completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   # instead of line above, also generated intiially as:
   # get "posts/index"
   # get "posts/show"
