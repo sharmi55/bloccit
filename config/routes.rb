1caebf1987def77c0b41e682900251d743471598
Sharmi <rai.sharmistha@gmail.com>
2016-03-15 12:24:25 +0100

    Assignemnt 37: completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  # instead of line above, also generated intiially as:
-  # get "posts/index"
-  # get "posts/show"
-  # get "posts/new"
-  # get "posts/edit"
   post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
-# instead of line above, also generated initially as:
-# get "welcome/index"
-# get "welcome/about"
 
   root 'welcome#index'
 
