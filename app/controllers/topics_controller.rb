d37b5082895f2be879d0293f7f25d4b2b7e68c52
Sharmi <rai.sharmistha@gmail.com>
2016-03-16 16:38:28 +0100

    Checkpoint 39: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,10 +13,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic = Topic.new(topic_params)
 
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -32,10 +29,8 @@ class TopicsController < ApplicationController
 
   def update
     @topic = Topic.find(params[:id])
+    @topic.assign_attributes(topic_params)
 
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
 
     if @topic.save
        flash[:notice] = "Topic was updated."
@@ -58,4 +53,11 @@ class TopicsController < ApplicationController
      end
    end
 
+
+
+  private
+   
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
-#require 'random_data'
+5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
 
-# Create Topics
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -12,6 +18,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user:   users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -28,7 +35,14 @@ posts = Post.all
   )
 end
 
-puts "Seed finished"
-puts "#{Topic.count} topics created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+user = User.first
+ user.update_attributes!(
+   email: 'pompi_rai@hotmail.com', 
+   password: 'helloworld'
+ )
+
+  puts "Seed finished"
+  puts "#{User.count} users created"
+  puts "#{Topic.count} topics created"
+  puts "#{Post.count} posts created"
+  puts "#{Comment.count} comments created"
