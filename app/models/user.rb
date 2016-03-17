464b81d0038c70440f4d936de61dea7affe61244
Sharmi <rai.sharmistha@gmail.com>
2016-03-17 12:46:10 +0100

    Checkpoint 40 authorization: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,18 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
-# #3
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #4
- validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
- validates :password, length: { minimum: 6 }, allow_blank: true
-# #5
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
-# #6
   has_secure_password
+
+  enum role: [:member, :admin]
+
 end
