3aeb0f02492d402ef131735e14ccd7108e53bd0a
Sharmi <rai.sharmistha@gmail.com>
2016-03-23 15:26:01 +0100

    checkpoint 47 ajax: completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,35 +3,41 @@ class CommentsController < ApplicationController
   before_action :require_sign_in
   before_action :authorize_user, only: [:destroy]
 
-   def create
-     @post = Post.find(params[:post_id])
-     comment = @post.comments.new(comment_params)
-     comment.user = current_user
-
-     if comment.save
-       flash[:notice] = "Comment saved successfully."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment failed to save."
-       redirect_to [@post.topic, @post]
-     end
-   end
-
-  def destroy
+  def create
     @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    @new_comment = Comment.new
 
-    if comment.destroy
-      flash[:notice] = "Comment was deleted."
-      redirect_to [@post.topic, @post]
+    if @comment.save
+      flash[:notice] = "Comment saved successfully."
     else
-      flash[:alert] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
+      flash[:alert] = "Comment failed to save."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
-    private
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
 
+    if @comment.destroy
+      flash[:notice] = "Comment was deleted."
+    else
+      flash[:alert] = "Comment couldn't be deleted. Try again."
+    end
+
+    respond_to do |format|
+       format.html
+       format.js
+     end
+   end
+
+  private
     def comment_params
       params.require(:comment).permit(:body)
     end
