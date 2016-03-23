b069041a32e47626c5add6db13c3056568a60120
Sharmi <rai.sharmistha@gmail.com>
2016-03-23 12:37:49 +0100

    checkpoint 46 completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,21 +7,40 @@ RSpec.describe Topic, type: :model do
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
 
-  describe "attributes" do
-    it "responds to name" do
-      expect(topic).to respond_to(:name)
-    end
+    describe "scopes" do
+      before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+      end
 
-    it "responds to description" do
-      expect(topic).to respond_to(:description)
-    end
+    describe "visible_to(user)" do
 
-    it "responds to public" do
-      expect(topic).to respond_to(:public)
-    end
+      it "returns all topics if the user is present" do
+        user = User.new
+          expect(Topic.visible_to(user)).to eq(Topic.all)
+        end
 
-    it "is public by default" do
-      expect(topic.public).to be(true)
+      it "returns only public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
     end
   end
-end
+    
+    describe "attributes" do
+      it "responds to name" do
+        expect(topic).to respond_to(:name)
+      end
+
+      it "responds to description" do
+        expect(topic).to respond_to(:description)
+      end
+
+      it "responds to public" do
+        expect(topic).to respond_to(:public)
+      end
+
+      it "is public by default" do
+        expect(topic.public).to be(true)
+      end
+    end
+  end
